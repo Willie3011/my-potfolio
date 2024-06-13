cc68f0075c35380574e5d6c616a77748307edb9d
Buddynice34 <willemapril33@gmail.com>
2024-06-13 12:29:51 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <h1>Willem's Potfolio</h1>
+      <p>Let's do this</p>
+      <button className="w-fit text-white font-bold py-2 px-4 rounded-3xl bg-cyan-500">Start now</button>
+    </>
+  )
+}
+
+export default App
