f14bf86f7e942c65989a9dbe870e8c3828dde94c
Buddynice34 <willemapril33@gmail.com>
2024-06-17 14:21:25 +0200

    feature: created a header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Navbar from '../../components/Navbar/Navbar'
 import Banner from '../../sections/Banner/Banner'
 import Skills from '../../sections/Skills/Skills'
-
+import Header from '../../components/Header/Header'
 function Home() {
   return (
     <div className="h-auto">
         <Navbar/>
+        <Header/>
         <Banner/>
         <Skills/>
     </div>
